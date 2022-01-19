d27cf052b14c49dd2ac9a3860e04e188424ddf37
verowangari <verowangari34@gmail.com>
2022-01-19 16:14:14 +0300

    style the navbar with a green background color

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+//const showMenu = (toggleId, navId) =>{
+  //  const toggle = document.getElementById(toggleId),
+    //nav = document.getElementById(navId)
     
     // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    //if(toggle && nav){
+      //  toggle.addEventListener('click', ()=>{
             // We add the show-menu class to the div tag with the nav__menu class
-            nav.classList.toggle('show-menu')
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu')
+        //    nav.classList.toggle('show-menu')
+        //})
+    //}
+//}
+//showMenu('nav-toggle','nav-menu')
+let menu= document.querySelector(#menu-bars)
+
